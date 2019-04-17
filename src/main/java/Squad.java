14f607025ea312f02140b6811b0565b55c8d8493
mikewanya <wanyamalately@gmail.com>
2019-04-17 12:34:50 +0300

    cleaup

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -2,5 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Squad {
+    private String mName;
+    private String mAge;
+    private String mSuperpower;
+    private String mWeakness;
+    private String mCause;
+
+    public Squad(String name, String age, String superpower, String weakness ,String cause) {
+        mName = name;
+        mAge = age;
+        mSuperpower = superpower;
+        mWeakness = weakness;
+        mCause= cause;
+    }
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmAge() {
+        return mAge;
+    }
+
+    public String getmSuperpower() {
+        return mSuperpower;
+    }
+
+    public String getmWeakness() {
+        return mWeakness;
+    }
+
+    public String getmCause() {
+        return mCause;
+    }
+
 
 }
+
